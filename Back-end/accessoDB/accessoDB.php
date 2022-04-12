47a9c7179a70a496db8598c2b7a2ae07ee0b2cb5
Tommaso <tbianchi866@gmail.com>
2022-04-13 00:27:52 +0200

    Back-End

---
+++ Back-end/accessoDB/accessoDB.php
@@ -0,0 +1,33 @@
+<?php
+
+class accessoDB
+{
+      private $nomeServer;
+      private $nomeUtente;
+      private $password;
+      private $nomeDatabase;
+      private $connessione;
+
+    public function __construct()
+   {
+      $this->nomeServer = "172.17.0.1";
+      $this->nomeUtente = "root";
+      $this->password = "1234";
+      $this->nomeDatabase = "mydb";
+   }
+   
+   public function OpenCon()
+   {      
+      //Connessione Con Il DataBase
+
+      $this->connessione = mysqli_connect($this->nomeServer, $this->nomeUtente, $this->password, $this->nomeDatabase);
+   
+      return $this->connessione;
+   }
+   
+   public function CloseCon($c)
+   {
+      $c->close();
+   }
+}
+?>
