a7fd4b81cbacdc1c109455137d54dd48455b93a8
Bianchi-Tommaso <73885689+Bianchi-Tommaso@users.noreply.github.com>
2022-05-29 10:48:14 +0200

    -

--- Front-end/js/js.js
+++ Front-end/js/js.js
@@ -1,36 +1,78 @@
 //var index = "http://192.168.1.48:8081/pages/backend.php";
 var index = "http://localhost:8080/pages/backend.php";
-var self, next, last, prev, page, json, totalPages, idModifica;
-
-$(document).ready(function () 
-{
+var self, next, last, prev, page, json, totalPages, idModifica, editor;
 
   $("body").ready(function () 
   {
     GET(index + "?start=0&length=20");
   });
 
- 
-
-  function GET(link)   //prende i dati e stampa i dipendeti
+  
+  function GET(link)
   {
-    $(document).ready(function() {
-      $('#example').DataTable( {
-          "processing": true,
-          "serverSide": true,
-          "ajax": {
-              "url": index,
-              "type": "POST"
-          },
-          "columns": [
-              { "data": "id" },
-              { "data": "birth_date" },
-              { "data": "first_name" },
-              { "data": "last_name" },
-              { "data": "gender" },
-              { "data": "hire_date" }
+
+      editor = new $.fn.dataTable.Editor( {
+          ajax: link,
+          table: "#example",
+          fields: [ {
+                  label: "First name:",
+                  name: "users.first_name"
+              }, {
+                  label: "Last name:",
+                  name: "users.last_name"
+              }, {
+                  label: "Phone #:",
+                  name: "users.phone"
+              }, {
+                  label: "Site:",
+                  name: "users.site",
+                  type: "select"
+              }, {
+                  name: "users.removed_date",
+                  type: "hidden"
+              }
           ]
       } );
-  } );
-  };
-});
+   
+      var table = $('#example').DataTable( {
+          dom: "Bfrtip",
+          ajax: {
+              url: link,
+              type: 'POST'
+          },
+          columns: [
+              { data: "users.first_name" },
+              { data: "users.last_name" },
+              { data: "users.phone" },
+              { data: "sites.name" }
+          ],
+          select: true,
+          buttons: [
+              { extend: "create", editor: editor },
+              { extend: "edit",   editor: editor },
+              {
+                  extend: "selected",
+                  text: 'Delete',
+                  action: function ( e, dt, node, config ) {
+                      var rows = table.rows( {selected: true} ).indexes();
+   
+                      editor
+                          .hide( editor.fields() )
+                          .one( 'close', function () {
+                              setTimeout( function () { // Wait for animation
+                                  editor.show( editor.fields() );
+                              }, 500 );
+                          } )
+                          .edit( rows, {
+                              title: 'Delete',
+                              message: rows.length === 1 ?
+                                  'Are you sure you wish to delete this row?' :
+                                  'Are you sure you wish to delete these '+rows.length+' rows',
+                              buttons: 'Delete'
+                          } )
+                          .val( 'users.removed_date', (new Date()).toISOString().split('T')[0] );
+                  }
+              }
+          ]
+      } );
+}
